0739dbcfe62a51d1369b26bbb78f38c1256948ad
zerrinabak <software.developer.01212@gmail.com>
2024-10-13 18:27:38 +0300

    Project files added.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import "bulma/css/bulma.css";
+import "./App.css";
+import Course from "./Course";
+import Flowers from "./images/flowers.jpg";
+import Summer from "./images/summer.jpg";
+import Winter from "./images/winter.jpg";
+import Sunset from "./images/sunset.jpg";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section class="hero is-link">
+        <div class="hero-body">
+          <p class="title">CARDS</p>
+        </div>
+      </section>
+      <section className="section"></section>
+      <div className="container">
+        <div className="columns">
+          <div className="column">
+            <Course
+              image={Flowers}
+              title="Flowers"
+              description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. In nibh est, gravida non massa eu, consectetur feugiat turpis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam mollis id dolor bibendum blandit. Maecenas eu est et massa volutpat vehicula vitae vitae libero. Ut volutpat sapien nunc, ac consectetur diam venenatis quis. Aenean leo ligula, mattis sit amet lacinia sed, gravida in nunc. Nullam eu orci quam."
+            />
+          </div>
+          <div className="column">
+            <Course
+              image={Summer}
+              title="Summer"
+              description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. In nibh est, gravida non massa eu, consectetur feugiat turpis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam mollis id dolor bibendum blandit. Maecenas eu est et massa volutpat vehicula vitae vitae libero. Ut volutpat sapien nunc, ac consectetur diam venenatis quis. Aenean leo ligula, mattis sit amet lacinia sed, gravida in nunc. Nullam eu orci quam."
+            />
+          </div>
+          <div className="column">
+            <Course
+              image={Winter}
+              title="Winter"
+              description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. In nibh est, gravida non massa eu, consectetur feugiat turpis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam mollis id dolor bibendum blandit. Maecenas eu est et massa volutpat vehicula vitae vitae libero. Ut volutpat sapien nunc, ac consectetur diam venenatis quis. Aenean leo ligula, mattis sit amet lacinia sed, gravida in nunc. Nullam eu orci quam."
+            />
+          </div>
+          <div className="column">
+            <Course
+              image={Sunset}
+              title="Sunset"
+              description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. In nibh est, gravida non massa eu, consectetur feugiat turpis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam mollis id dolor bibendum blandit. Maecenas eu est et massa volutpat vehicula vitae vitae libero. Ut volutpat sapien nunc, ac consectetur diam venenatis quis. Aenean leo ligula, mattis sit amet lacinia sed, gravida in nunc. Nullam eu orci quam."
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
